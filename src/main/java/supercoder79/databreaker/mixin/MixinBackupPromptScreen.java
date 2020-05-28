974aed5c73b0bfecf1ed954f6004799c9be652b5
SuperCoder7979 <k.pranav@gmail.com>
2020-05-28 14:08:21 -0400

    Update MixinBackupPromptScreen.java

--- src/main/java/supercoder79/databreaker/mixin/MixinBackupPromptScreen.java
+++ src/main/java/supercoder79/databreaker/mixin/MixinBackupPromptScreen.java
@@ -4,8 +4,10 @@ import net.minecraft.client.gui.screen.BackupPromptScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CheckboxWidget;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,13 +25,25 @@ public class MixinBackupPromptScreen extends Screen {
 
     @Shadow @Final private Screen parent;
 
+    @Shadow @Final protected BackupPromptScreen.Callback callback;
+
+    @Shadow private CheckboxWidget eraseCacheCheckbox;
+
+    @Shadow @Final private boolean showEraseCacheCheckbox;
+
     protected MixinBackupPromptScreen(Text title) {
         super(title);
     }
 
+    @Unique boolean dontStop;
+
     @Inject(method = "<init>", at = @At("RETURN"))
     private void hookConstructor(Screen parent, BackupPromptScreen.Callback callback, Text title, Text subtitle, boolean showEraseCacheCheckBox, CallbackInfo ci) {
-        this.subtitle = new LiteralText("You cannot load older worlds with DataBreaker installed. Please remove DataBreaker to load the world.");
+        if (title.toString().contains("selectWorld.backupQuestion.experimental")) {
+            dontStop = true;
+        } else {
+            this.subtitle = new LiteralText("You cannot load older worlds with DataBreaker installed. Please remove DataBreaker to load the world.");
+        }
     }
 
     /**
@@ -37,14 +51,34 @@ public class MixinBackupPromptScreen extends Screen {
      */
     @Overwrite
     public void init() {
-        this.wrappedText.clear();
-        this.wrappedText.addAll(this.textRenderer.wrapLines(this.subtitle, this.width - 50));
-        int var10000 = this.wrappedText.size() + 1;
-        this.textRenderer.getClass();
-        int i = var10000 * 9;
-        this.addButton(new ButtonWidget(this.width / 2 - 155 + 80, 124 + i, 150, 20, ScreenTexts.CANCEL, (buttonWidget) -> {
-            this.client.openScreen(this.parent);
-        }));
-
+        if (!dontStop) {
+            this.wrappedText.clear();
+            this.wrappedText.addAll(this.textRenderer.wrapLines(this.subtitle, this.width - 50));
+            int var10000 = this.wrappedText.size() + 1;
+            this.textRenderer.getClass();
+            int i = var10000 * 9;
+            this.addButton(new ButtonWidget(this.width / 2 - 155 + 80, 124 + i, 150, 20, ScreenTexts.CANCEL, (buttonWidget) -> {
+                this.client.openScreen(this.parent);
+            }));
+        } else {
+            this.wrappedText.clear();
+            this.wrappedText.addAll(this.textRenderer.wrapLines(this.subtitle, this.width - 50));
+            int var10000 = this.wrappedText.size() + 1;
+            this.textRenderer.getClass();
+            int i = var10000 * 9;
+            this.addButton(new ButtonWidget(this.width / 2 - 155, 100 + i, 150, 20, new TranslatableText("selectWorld.backupJoinConfirmButton"), (buttonWidget) -> {
+                this.callback.proceed(true, this.eraseCacheCheckbox.isChecked());
+            }));
+            this.addButton(new ButtonWidget(this.width / 2 - 155 + 160, 100 + i, 150, 20, new TranslatableText("selectWorld.backupJoinSkipButton"), (buttonWidget) -> {
+                this.callback.proceed(false, this.eraseCacheCheckbox.isChecked());
+            }));
+            this.addButton(new ButtonWidget(this.width / 2 - 155 + 80, 124 + i, 150, 20, ScreenTexts.CANCEL, (buttonWidget) -> {
+                this.client.openScreen(this.parent);
+            }));
+            this.eraseCacheCheckbox = new CheckboxWidget(this.width / 2 - 155 + 80, 76 + i, 150, 20, new TranslatableText("selectWorld.backupEraseCache"), false);
+            if (this.showEraseCacheCheckbox) {
+                this.addButton(this.eraseCacheCheckbox);
+            }
+        }
     }
 }

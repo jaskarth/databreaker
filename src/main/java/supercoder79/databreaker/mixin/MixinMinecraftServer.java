596c5e3cb8d887bbc1b83dc152aed9d53f7699ec
IMS212 <ims@ims21.tech>
2021-10-22 22:06:58 -0700

    Upgrade to 21w42a

--- src/main/java/supercoder79/databreaker/mixin/MixinMinecraftServer.java
+++ src/main/java/supercoder79/databreaker/mixin/MixinMinecraftServer.java
@@ -1,23 +1,25 @@
 package supercoder79.databreaker.mixin;
 
+import com.google.common.collect.ImmutableSet;
 import com.mojang.datafixers.DataFixer;
+import net.minecraft.server.Main;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import net.minecraft.world.level.storage.LevelStorage;
+import net.minecraft.world.updater.WorldUpdater;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BooleanSupplier;
 
-@Mixin(MinecraftServer.class)
+@Mixin(Main.class)
 public abstract class MixinMinecraftServer {
-    @Redirect(method = "convertLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/LevelStorage$Session;needsConversion()Z"))
-    private static boolean pickUpBrokenGlass_convertLevel(LevelStorage.Session session) {
-        boolean shouldExplode = session.needsConversion();
-        if (shouldExplode) {
+    @Inject(method = "forceUpgradeWorld", at = @At("HEAD"))
+    private static void pickUpBrokenGlass_convertLevel(LevelStorage.Session session, DataFixer dataFixer, boolean eraseCache, BooleanSupplier booleanSupplier, ImmutableSet<RegistryKey<World>> worlds, CallbackInfo ci) {
             throw new RuntimeException("You cannot upgrade worlds with DataBreaker. Please remove DataBreaker and then upgrade your world.");
-        }
-
-        return false;
     }
 }
